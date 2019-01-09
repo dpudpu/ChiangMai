0233db2914296a0da7d4e680991fd2d728fd7961
DaejunBae <dpudpu11@gmail.com>
2019-01-09 17:51:52 +0900

    Init Project

---
+++ src/main/java/com/jmt/ChiangMai/ChiangMaiApplication.java
@@ -0,0 +1,14 @@
+package com.jmt.ChiangMai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiangMaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiangMaiApplication.class, args);
+	}
+
+}
+
