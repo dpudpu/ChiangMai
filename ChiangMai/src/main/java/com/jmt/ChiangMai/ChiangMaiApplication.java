e6aa6113438986559e07366ffc7d06e014ba8476
DaejunBae <dpudpu11@gmail.com>
2019-01-10 15:22:30 +0900

    Modify 경로

--- ChiangMai/src/main/java/com/jmt/ChiangMai/ChiangMaiApplication.java
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/ChiangMaiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChiangMaiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChiangMaiApplication.class, args);
 	}
