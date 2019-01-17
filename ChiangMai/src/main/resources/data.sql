0c1872f3da75e1618fdbe81cafa0ef32d7aca99e
DaejunBae <dpudpu11@gmail.com>
2019-01-17 18:41:20 +0900

    Modify sample data

--- ChiangMai/src/main/resources/data.sql
+++ ChiangMai/src/main/resources/data.sql
@@ -4,9 +4,9 @@ insert into role (id,  name) values (2, 'ADMIN');
 
 -- 회원
 INSERT INTO member (id, email, gender, nickname, password) VALUES (1, 'dpudpu11@gmail.com', '남자', '배대준','1234');
-INSERT INTO member (id, email, gender, nickname, password) VALUES (2, 'dpudpu@gmail.com', '남자', '대준','1234');
+INSERT INTO member (id, email, gender, nickname, password) VALUES (2, 'asdf@gmail.com', '남자', '배대','1234');
 INSERT INTO member (id, email, gender, nickname, password) VALUES (3, 'dpu@gmail.com', '남자', '3','1234');
-INSERT INTO member (id, email, gender, nickname, password) VALUES (4, 'd@gmail.com', '남자', '4','1234');
+INSERT INTO member (id, email, gender, nickname, password) VALUES (4, 'd@gmail.com', '남자', '대준','1234');
 INSERT INTO member (id, email, gender, nickname, password) VALUES (5, 'dp@gmail.com', '남자', '5','1234');
 
 
