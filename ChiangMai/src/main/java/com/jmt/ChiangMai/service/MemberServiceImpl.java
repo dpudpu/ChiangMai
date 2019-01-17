87e81175867068e90335ece9c5691c89c943c72f
DaejunBae <dpudpu11@gmail.com>
2019-01-17 18:06:51 +0900

    Add MemberService

---
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/service/MemberServiceImpl.java
@@ -0,0 +1,103 @@
+package com.jmt.ChiangMai.service;
+
+import com.jmt.ChiangMai.domain.Member;
+import com.jmt.ChiangMai.domain.Role;
+import com.jmt.ChiangMai.repository.MemberRepository;
+import com.jmt.ChiangMai.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+    private final RoleRepository roleRepository;
+
+
+    @Override
+    @Transactional
+    public Member signUp(Member member) {
+        Member memberInfo = memberRepository.save(member);
+
+        // 패스워드 인코딩
+
+
+        Set<Role> roles = roleRepository.findByName("USER");
+        memberInfo.setRoles(roles);
+
+        return memberInfo;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Member login(String email, String password) {
+        return memberRepository.findByEmailAndPassword(email, password);
+    }
+
+    @Override
+    @Transactional
+    public void withdrawMember(String email, String password) {
+        Member member = memberRepository.findByEmailAndPassword(email, password);
+        if (member != null)
+            memberRepository.delete(member);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean checkNickname(String nickname) {
+        return memberRepository.existsMemberByNickname(nickname);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean checkEmail(String email) {
+        return memberRepository.existsMemberByEmail(email);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean checkPassword(String password) {
+        return memberRepository.existsMemberByPassword(password);
+    }
+
+    @Override
+    @Transactional
+    public Member modifyMemberInfo(Member member) {
+        Member memberId = memberRepository.getOne(member.getId());
+        BeanUtils.copyProperties(member,memberId);
+        return memberId;
+    }
+
+    @Override
+    @Transactional
+    public int modifyMemberRoles(Long id, Long[] roles) {
+        return 0;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Member> searchMembers(Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Member> searchMembersByWord(String searchWord, String searchType, Pageable pageable) {
+        Page<Member> members = null;
+        switch (searchType) {
+            case "email":
+                members = memberRepository.findByEmailContaining(searchWord, pageable);
+                break;
+            case "nickname":
+                members = memberRepository.findByNicknameContaining(searchWord, pageable);
+                break;
+        }
+        return members;
+    }
+}
