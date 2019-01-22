ad3df634314569bc8d7cb1335a66a47ffea4d84e
DaejunBae <dpudpu11@gmail.com>
2019-01-22 19:20:14 +0900

    Add FileUploadException

---
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/exception/FileUploadException.java
@@ -0,0 +1,11 @@
+package com.jmt.ChiangMai.exception;
+
+public class FileUploadException extends RuntimeException {
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
