4cc3aab0053929d4e27111468fe062f2c40aef93
DaejunBae <dpudpu11@gmail.com>
2019-01-22 17:05:32 +0900

    Add ShopService

---
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/service/ShopService.java
@@ -0,0 +1,23 @@
+package com.jmt.ChiangMai.service;
+
+import com.jmt.ChiangMai.domain.Filter;
+import com.jmt.ChiangMai.domain.Shop;
+import com.jmt.ChiangMai.domain.ShopImages;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface ShopService {
+    List<Shop> getShops(Sort sort);
+
+    List<Shop> getShopsByFilters(List<String> types, List<Filter> filters, Pageable pageable);
+
+    void deleteShop(Long id);
+
+    void deleteShopImage(Long id);
+
+    Shop modifyShop(Shop shop);
+
+    Shop addShop(Shop shop);
+ }
