05be651420834ab2e213ce419478b2964f74b64a
DaejunBae <dpudpu11@gmail.com>
2019-01-22 12:19:49 +0900

    Modify @Repository 삭제

--- ChiangMai/src/main/java/com/jmt/ChiangMai/repository/MemberRepository.java
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/repository/MemberRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByEmailAndPassword(String email, String password);
--- ChiangMai/src/main/java/com/jmt/ChiangMai/repository/RoleRepository.java
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/repository/RoleRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Set<Role> findByName(String name);
 }
