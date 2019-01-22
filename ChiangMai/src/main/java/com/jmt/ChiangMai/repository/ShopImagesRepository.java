66c19da7fa8b434d2575bb7a82c2aa3a5e845f10
DaejunBae <dpudpu11@gmail.com>
2019-01-22 19:17:57 +0900

    Add ShopImagesRepository

---
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/repository/ShopImagesRepository.java
@@ -0,0 +1,8 @@
+package com.jmt.ChiangMai.repository;
+
+import com.jmt.ChiangMai.domain.ShopImages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopImagesRepository extends JpaRepository<ShopImages, Long> {
+
+}
