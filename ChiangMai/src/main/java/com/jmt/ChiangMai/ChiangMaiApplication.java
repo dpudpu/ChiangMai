41a71bc41260af1744ea46ef95177e5e073740eb
DaejunBae <dpudpu11@gmail.com>
2019-01-22 19:19:45 +0900

    Add @EnableConfigurationProperties

--- ChiangMai/src/main/java/com/jmt/ChiangMai/ChiangMaiApplication.java
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/ChiangMaiApplication.java
@@ -1,9 +1,14 @@
 package com.jmt.ChiangMai;
 
+import com.jmt.ChiangMai.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class ChiangMaiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChiangMaiApplication.class, args);
