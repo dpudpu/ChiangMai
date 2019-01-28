1bd930fd9d1c069d3d4a12c7f1d4fb450f565146
DaejunBae <dpudpu11@gmail.com>
2019-01-28 19:47:37 +0900

    Add LoginFailureHandler 로그인 실패시 email, error 메시지 전송

---
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/security/LoginFailureHandler.java
@@ -0,0 +1,21 @@
+package com.jmt.ChiangMai.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.sendRedirect("/members/login?failure=true&email="+request.getParameter("email"));
+
+        // 왜 POST로 전송되지? (Request method 'POST' not supported)
+//        request.setAttribute("email",request.getParameter("email"));
+//        request.setAttribute("failure",true);
+//        request.getRequestDispatcher("/members/login").forward(request,response);
+    }
+}
