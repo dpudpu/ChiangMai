27cb703a0f4437a651d14ffa7ce6adbaad5ee4d2
DaejunBae <dpudpu11@gmail.com>
2019-02-15 18:00:05 +0900

    Test shopRepository join fetch

--- ChiangMai/src/test/java/com/jmt/ChiangMai/repository/ShopRepositoryTest.java
+++ ChiangMai/src/test/java/com/jmt/ChiangMai/repository/ShopRepositoryTest.java
@@ -1,26 +1,29 @@
-//package com.jmt.ChiangMai.repository;
-//
-//import com.jmt.ChiangMai.domain.Filter;
-//import com.jmt.ChiangMai.domain.Shop;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//@Transactional
-//public class ShopRepositoryTest {
-//
-//    @Autowired
-//    ShopRepository shopRepository;
+package com.jmt.ChiangMai.repository;
+
+import com.jmt.ChiangMai.domain.Filter;
+import com.jmt.ChiangMai.domain.Review;
+import com.jmt.ChiangMai.domain.Shop;
+import com.jmt.ChiangMai.domain.ShopImage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Transactional
+public class ShopRepositoryTest {
+
+    @Autowired
+    ShopRepository shopRepository;
 //
 //    @Test
 //    public void 타입_검색() {
@@ -36,7 +39,7 @@
 //            System.out.println(s.getName());
 //        }
 //    }
-//
+
 //    @Test
 //    public void 영업중_검색() {
 //        List<Shop> shops = shopRepository.findByOpenAfterAndCloseBefore(15L);
@@ -62,4 +65,17 @@
 //                System.out.println(f.getName());
 //        }
 //    }
-//}
+
+    @Test
+    public void 전체검색(){
+        Pageable pageable = PageRequest.of(0,2000,Sort.Direction.DESC,"id");
+        Page<Shop>  shops = shopRepository.findAll(pageable);
+        int i=0;
+        for(Shop s : shops){
+            i++;
+            System.out.println(i+": "+s.getName()+"  / ");
+            for(Review si : s.getReviews())
+                System.out.println(si.getContent());
+        }
+    }
+}
