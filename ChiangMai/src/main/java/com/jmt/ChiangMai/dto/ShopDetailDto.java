e6ba130e044961977f420e79a780f63139c8f171
DaejunBae <dpudpu11@gmail.com>
2019-02-18 20:46:23 +0900

    Add ShopDetailDto

---
+++ ChiangMai/src/main/java/com/jmt/ChiangMai/dto/ShopDetailDto.java
@@ -0,0 +1,34 @@
+package com.jmt.ChiangMai.dto;
+
+import com.jmt.ChiangMai.domain.Filter;
+import com.jmt.ChiangMai.domain.Review;
+import com.jmt.ChiangMai.domain.ShopImage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShopDetailDto {
+    private Long id;
+    private String type;
+    private String name;
+    private String address;
+    private Double lat;
+    private Double lng;
+    private String phone;
+    private Long open;
+    private Long close;
+    private String content;
+    private float rating;
+    private int reviewTotal;
+    private Page<Review> reviews;
+    private Set<Filter> filters;
+    private ShopImage shopImage;
+}
